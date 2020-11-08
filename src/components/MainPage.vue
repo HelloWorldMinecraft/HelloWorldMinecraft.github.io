cc33c0f7d42039f60392bee479ff482626adb9d9
Username4723 <11415806+Username4723@users.noreply.github.com>
2020-11-07 20:50:46 -0600

    Add Title

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
+  <div style="margin-left: 10%; margin-right: 10%">
+    <b-card bg-variant="transparent" text-variant="light" border-variant="dark" title="Red's Server">
+    </b-card>
   </div>
 </template>
 
@@ -10,4 +12,8 @@ export default {
 </script>
 
 <style scoped>
+
+.card-body {
+  background-color: rgb(0, 0, 0, 0.6);
+}
 </style>
