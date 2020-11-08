09ffcc709bd9a00a10496deb421b0195d1459247
Username4723 <11415806+Username4723@users.noreply.github.com>
2020-11-08 16:57:48 -0600

    Split Features and Servers

---
+++ src/components/Features.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <b-row>
+      <b-col cols="3">
+        <b-card-img v-b-tooltip title="Image Source" src="TODO" alt="TODO"></b-card-img>
+      </b-col>
+      <b-col>
+        <b-card-title>TODO</b-card-title>
+        <b-card-sub-title>TODO</b-card-sub-title>
+        <b-card-text>TODO</b-card-text>
+      </b-col>
+    </b-row>
+  </div>
+</template>
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,59 +1,23 @@
 <template>
   <div style="margin-left: 10%; margin-right: 10%">
     <b-card bg-variant="transparent" text-variant="light" border-variant="dark">
-      <b-carousel controls indicators fade interval="3000">
-        <b-carousel-slide img-src="/screenshots/agskies2-1.png" caption="Agrarian Skies 2" text="Islands by Red3842 (Left) and Pika_trooper (Right)">
-          <b-button variant="success" href="#agskies2">Join Now!</b-button>
-        </b-carousel-slide>
+      <Servers/>
+    </b-card>
 
-        <b-carousel-slide img-src="/screenshots/crundee-1.png" caption="CrundeeCraft" text="Base by Hughster3 and Red3842">
-          <b-button variant="success" href="#crundee">Join Now!</b-button>
-        </b-carousel-slide>
-        <b-carousel-slide img-src="/screenshots/crundee-2.png" caption="CrundeeCraft" text="Base by Hughster3 and Red3842">
-          <b-button variant="success" href="#crundee">Join Now!</b-button>
-        </b-carousel-slide>
+    <br/>
 
-        <b-carousel-slide img-src="/screenshots/infinitysb-1.png" caption="FTB Infintiy Evolved Skyblock" text="Base by Red3842">
-          <b-button variant="success" href="#infinitysb">Join Now!</b-button>
-        </b-carousel-slide>
-        <b-carousel-slide img-src="/screenshots/infinitysb-2.png" caption="FTB Infintiy Evolved Skyblock" text="Base by Leaf_TreePlayers">
-          <b-button variant="success" href="#infinitysb">Join Now!</b-button>
-        </b-carousel-slide>
-
-
-
-        <b-carousel-slide img-src="/screenshots/pixelmon-1.png" caption="Pixelmon" text="Base by loonix28, Pika_trooper, and Red3842">
-          <b-button variant="success" href="#pixelmon">Join Now!</b-button>
-        </b-carousel-slide>
-
-        <b-carousel-slide img-src="/screenshots/prankpack2-1.png" caption="PrankPack 2" text="Base by Red3842">
-          <b-button variant="success" href="#prankpack2">Join Now!</b-button>
-        </b-carousel-slide>
-
-        <b-carousel-slide img-src="/screenshots/stoneblock-1.png" caption="Stoneblock" text="Base by Red3842">
-          <b-button variant="success" href="#stoneblock">Join Now!</b-button>
-        </b-carousel-slide>
-
-        <b-carousel-slide img-src="/screenshots/skyodyssey-1.png" caption="FTB Sky Odyssey" text="Base by Red3842">
-          <b-button variant="success" href="#skyodyssey">Join Now!</b-button>
-        </b-carousel-slide>
-
-
-
-        <b-carousel-slide img-src="/screenshots/creative-1.png" caption="Creative" text="Build by Hughster3, Youthier, _WindRunner_, smarshmallow, Dakingyuknow34, seeper20, and Xfer_Terminus">
-          <b-button variant="success" href="#creative">Join Now!</b-button>
-        </b-carousel-slide>
-
-        <b-carousel-slide img-src="/screenshots/survival2-1.png" caption="Survival 2" text="Build by _WindRunner_ and Dakingyuknow34">
-          <b-button variant="success" href="#survival2">Join Now!</b-button>
-        </b-carousel-slide>
-      </b-carousel>
+    <b-card id="" bg-variant="transparent" align="left" text-variant="light" border-variant="dark">
+      <Features/>
     </b-card>
   </div>
 </template>
 
 <script>
+import Servers from "./Servers.vue"
+import Features from "./Features.vue"
+
 export default {
+  components: { Servers, Features },
   name: 'MainPage'
 }
 </script>
