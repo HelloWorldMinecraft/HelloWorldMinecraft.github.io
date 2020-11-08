79ee568a0f2e3dea108ae0a3cf5b424ef2fefdea
Username4723 <11415806+Username4723@users.noreply.github.com>
2020-11-07 21:30:12 -0600

    Add carousel

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,6 +1,44 @@
 <template>
   <div style="margin-left: 10%; margin-right: 10%">
     <b-card bg-variant="transparent" text-variant="light" border-variant="dark" title="Red's Server">
+      <b-carousel controls indicators>
+        <b-carousel-slide img-src="/screenshots/agskies2-1.png" caption="Agrarian Skies 2" text="Islands by Red3842 (Left) and Pika_trooper (Right)">
+          <b-button variant="success" href="#agskies2">Join Now!</b-button>
+        </b-carousel-slide>
+
+        <b-carousel-slide img-src="/screenshots/crundee-1.png" caption="CrundeeCraft" text="Base by Hughster3 and Red3842">
+          <b-button variant="success" href="#crundee">Join Now!</b-button>
+        </b-carousel-slide>
+        <b-carousel-slide img-src="/screenshots/crundee-2.png" caption="CrundeeCraft" text="Base by Hughster3 and Red3842">
+          <b-button variant="success" href="#crundee">Join Now!</b-button>
+        </b-carousel-slide>
+
+        <b-carousel-slide img-src="/screenshots/infinitysb-1.png" caption="FTB Infintiy Evolved Skyblock" text="Base by Red3842">
+          <b-button variant="success" href="#infinitysb">Join Now!</b-button>
+        </b-carousel-slide>
+        <b-carousel-slide img-src="/screenshots/infinitysb-2.png" caption="FTB Infintiy Evolved Skyblock" text="Base by Leaf_TreePlayers">
+          <b-button variant="success" href="#infinitysb">Join Now!</b-button>
+        </b-carousel-slide>
+
+
+
+        <b-carousel-slide img-src="/screenshots/pixelmon-1.png" caption="Pixelmon" text="Base by loonix28, Pika_trooper, and Red3842">
+          <b-button variant="success" href="#pixelmon">Join Now!</b-button>
+        </b-carousel-slide>
+
+        <b-carousel-slide img-src="/screenshots/prankpack2-1.png" caption="PrankPack 2" text="Base by Red3842">
+          <b-button variant="success" href="#prankpack2">Join Now!</b-button>
+        </b-carousel-slide>
+
+        <b-carousel-slide img-src="/screenshots/stoneblock-1.png" caption="Stoneblock" text="Base by Red3842">
+          <b-button variant="success" href="#stoneblock">Join Now!</b-button>
+        </b-carousel-slide>
+
+        <b-carousel-slide img-src="/screenshots/skyodyssey-1.png" caption="FTB Sky Odyssey" text="Base by Red3842">
+          <b-button variant="success" href="#skyodyssey">Join Now!</b-button>
+        </b-carousel-slide>
+
+      </b-carousel>
     </b-card>
   </div>
 </template>
